165a6f5646793b153a291240c8c9c2d2c1a48ef5
BONEE4 <61917484+BONEE4@users.noreply.github.com>
2022-05-13 16:17:58 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,9 +3,8 @@ import { Bridge } from "discord-cross-hosting"
 const server = new Bridge({
     port: 3000,
     authToken: "bonero",
-    totalShards: 1,
+    totalShards: "auto",
     totalMachines: 1,
-    shardsPerCluster: 5,
     token: "NjgzMDQwNDYxNDM0Mzg4NTAx.GHJ0an.NJiFkZmUhY-7-87Mfz4fhZd2i-FaUgoIlinINM",
 })
 
