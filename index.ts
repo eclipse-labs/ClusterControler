c0ce921c9d2286fecd3fd9affb8d3b939cf6fec5
BONEE4 <61917484+BONEE4@users.noreply.github.com>
2022-05-15 04:10:33 -0300

    Removed readyEvent and add shardsPerCluster

--- index.ts
+++ index.ts
@@ -4,13 +4,10 @@ const server = new Bridge({
     port: 3000,
     authToken: "bonero",
     totalShards: "auto",
+    shardsPerCluster: 5,
     totalMachines: 1,
     token: "NjgzMDQwNDYxNDM0Mzg4NTAx.GHJ0an.NJiFkZmUhY-7-87Mfz4fhZd2i-FaUgoIlinINM",
 })
 
 server.on("debug", console.log)
 server.start()
-
-server.on("ready", url => {
-    console.log("Server is ready" + url);
-})
