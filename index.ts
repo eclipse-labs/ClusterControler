056325d8c4727cc365f78cae6932b894edf3a40b
BONEE4 <61917484+BONEE4@users.noreply.github.com>
2022-06-30 03:18:38 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { Bridge } from "discord-cross-hosting"
 import * as config from "./Config"
 
-let ready = Date.now()
 const server = new Bridge({
     port: config.port,
     authToken: config.authToken,
@@ -9,15 +8,6 @@ const server = new Bridge({
     shardsPerCluster: config.shardsPerCluster,
     totalMachines: config.totalMachines,
     token: config.token
-})
-
-server.on("clientRequest", (message, client) => {
-
-    // @ts-ignore
-    if (message?.requestStatus) message.reply({
-        memory: process.memoryUsage().rss,
-        uptime: ready ? Date.now() - ready : 0
-    })
 })
 
 server.on("debug", console.log)
